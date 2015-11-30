89e6fbcc509d58359af7399913e89f8f1e6522e0
nharringtonwasatch <nharrington@wasatchphotonics.com>
2015-11-30 09:59:13 -0500

    -n and -sv options out of py.test command line

--- tests/test_views.py
+++ tests/test_views.py
@@ -13,22 +13,22 @@ class TestBasicGraphInterface:
         assert form.width() == 800
         assert form.height() == 600
 
-#class TestPixmapBackedGraph:
-    #def test_graph_starts_with_default_text(self):
-        #form = views.PixmapBackedGraph()
-        #assert form.graphback.title.text() == "BLUE GRAPH"
+class TestPixmapBackedGraph:
+    def test_graph_starts_with_default_text(self):
+        form = views.PixmapBackedGraph()
+        assert form.graphback.title.text() == "BLUE GRAPH"
 
-    #def test_graph_starts_with_default_icon(self):
-        #form = views.PixmapBackedGraph()
-#
-        #icon = form.graphback.icon.boundingRect()
-        #assert icon.width() == 23
-        #assert icon.height() == 23 
-    #
-    #def test_min_max_text_starts_with_default(self):
-        #form = views.PixmapBackedGraph()
-        #assert form.graphback.minimum.text == "123.45"
-        #assert form.graphback.maximum.text == "987.65"
+    def test_graph_starts_with_default_icon(self):
+        form = views.PixmapBackedGraph()
+
+        icon = form.graphback.icon.boundingRect()
+        assert icon.width() == 23
+        assert icon.height() == 23 
+    
+    def test_min_max_text_starts_with_default(self):
+        form = views.PixmapBackedGraph()
+        assert form.graphback.minimum.text == "123.45"
+        assert form.graphback.maximum.text == "987.65"
 
     #def test_play_button_starts_in_play_mode(self):
         #form = views.PixmapBackedGraph()
